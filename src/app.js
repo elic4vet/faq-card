94dde4c005508b02e5ff0e0e4628e8f1612b48ef
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-18 20:39:01 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 let acc = document.getElementsByClassName("accordion");
 let i;
+let currentPanel = null;
 
 for (i = 0; i < acc.length; i++) {
+  // Loop through all accordion buttons and add event listeners to them
   acc[i].addEventListener("click", function () {
+    // When the user clicks on an accordion button, toggle between hiding and showing the active panel
+    // Close the current active panel
+    if (currentPanel && currentPanel != this.nextElementSibling) {
+      currentPanel.style.maxHeight = null;
+      currentPanel.previousElementSibling.classList.remove("active");
+    }
+
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
+
     if (panel.style.maxHeight) {
+      // If the panel is open, close it
       panel.style.maxHeight = null;
     } else {
+      // If the panel is closed, open it
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
+
+    currentPanel = panel.style.maxHeight ? panel : null;
   });
 }
